23e24da44fb47e71fe0c657886d810c62c09ad9b
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-12-15 20:04:45 +0000

    added planets and people model

--- src/models.py
+++ src/models.py
@@ -11,18 +11,55 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-class People(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+    
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    birth_year = db.Column(db.String(120), unique=True, nullable=False)
+    gender = db.Column(db.String(120), unique=True, nullable=False)
+    height = db.Column(db.String(120), unique=True, nullable=False)
+    skin_color = db.Column(db.String(120), unique=True, nullable=False)
+    eye_color = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<People %r>' % self.username
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "height": self.height,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    climate = db.Column(db.String(120), unique=True, nullable=False)
+    population = db.Column(db.Integer, unique=True, nullable=False)
+    orbital_period = db.Column(db.String(120), unique=True, nullable=False)
+    rotation_period = db.Column(db.String(120), unique=True, nullable=False)
+    diameter = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.username
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "population": self.population,
+            "orbital_period": self.orbital_period,
+            "rotation_period": self.rotation_period,
+            "diameter": self.diameter,
         }
