6369a565a79dc02cbc5db16dbf4f5be2fcad2b65
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-12-16 19:47:41 +0000

    working on src/app.py

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People
 #from models import Person
 
 app = Flask(__name__)
@@ -38,6 +38,15 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
+
+    response_body = {
+        "msg": "Hello, this is your GET /user response "
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/people', methods=['GET'])
+def get_people():
 
     response_body = {
         "msg": "Hello, this is your GET /user response "
